04bcdeefa5a1ec502ea3099be524f428c372a76f
Dora2 <42214504+dora2-iOS@users.noreply.github.com>
2020-01-09 16:09:49 +0900

    Reject debug-enabled patches

--- macos/UNTETHERED/make_iBoot.sh
+++ macos/UNTETHERED/make_iBoot.sh
@@ -18,7 +18,7 @@ cd iboottmp/
 unzip -j ../$1 Firmware/all_flash/all_flash.$device.production/iBoot*
 mv -v iBoot* tmp
 ../bin/xpwntool tmp ibot.dec -iv $3 -k $5
-../bin/iBoot32Patcher ibot.dec ibot.pwned --rsa --debug -b "-v" --boot-partition --boot-ramdisk
+../bin/iBoot32Patcher ibot.dec ibot.pwned --rsa -b "-v" --boot-partition --boot-ramdisk
 ../bin/xpwntool ibot.pwned iBoot -t tmp
 echo "0000010: 6365" | xxd -r - iBoot
 echo "0000020: 6365" | xxd -r - iBoot
